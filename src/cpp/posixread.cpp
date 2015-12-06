99e4e240c8a7ce961c839f9f1b7955870c08d41b
Adrien Vergé <adrienverge@gmail.com>
2015-12-06 10:18:54 +0100

    Initial commit

---
+++ src/cpp/posixread.cpp
@@ -0,0 +1,269 @@
+/*
+ * Copyright (c) 2015 Adrien Vergé
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+#include <errno.h>
+#include <string.h>
+#include <unistd.h>
+
+#include <nan.h>
+
+/*
+ * Try to check a given argument is actually a `net.Socket` instance. This is
+ * not a strict check and can be easily be fooled. But at least, it should
+ * prevent some trivial programming errors.
+ */
+static bool looks_like_a_socket(v8::Local<v8::Value> object) {
+    if (!object->IsObject())
+        return false;
+    v8::Local<v8::Object> socket = object.As<v8::Object>();
+
+    v8::Local<v8::String> className = socket->GetConstructorName();
+    if (strcmp("Socket", *Nan::Utf8String(className->ToString())))
+        return false;
+
+    return true;
+}
+
+/*
+ * Checks if the socket has the 'readable' property set to true.
+ */
+static bool socket_is_readable(v8::Local<v8::Object> socket) {
+    v8::Local<v8::String> key =
+            Nan::New<v8::String>("readable").ToLocalChecked();
+
+    if (!socket->Has(key))
+        return false;
+
+    v8::Local<v8::Value> value = socket->Get(key);
+    return value->IsBoolean() && Nan::To<bool>(value).FromJust();
+}
+
+/*
+ * Make sure the passed socket has a TCP handle with an associated fd. Returns
+ * the fd on success, or -1 in case of error.
+ */
+static int get_fd_from_socket(v8::Local<v8::Object> socket) {
+    v8::Local<v8::String> key;
+    v8::Local<v8::Object> handle;
+    v8::Local<v8::Value> value;
+    v8::Local<v8::String> className;
+
+    key = Nan::New<v8::String>("_handle").ToLocalChecked();
+    if (!socket->Has(key))
+        return -1;
+
+    value = socket->Get(key);
+    if (!value->IsObject())
+        return -1;
+    handle = value.As<v8::Object>();
+
+    className = handle->GetConstructorName();
+    if (strcmp("TCP", *Nan::Utf8String(className->ToString())))
+        return -1;
+
+    key = Nan::New<v8::String>("fd").ToLocalChecked();
+    if (!handle->Has(key))
+        return -1;
+
+    value = handle->Get(key);
+    if (!value->IsNumber())
+        return -1;
+
+    int fd = Nan::To<int>(value).FromJust();
+
+    if (fd < 0)
+        return -1;
+
+    return fd;
+}
+
+class PosixReadWorker : public Nan::AsyncWorker {
+ private:
+    int fd;
+    bool fd_was_non_blocking;
+
+    size_t size;
+    v8::Local<v8::Object> buffer;
+    char *data;
+
+    /*
+     * Set the socket blocking, if it was not.
+     */
+    int set_blocking() {
+        int opts = fcntl(fd, F_GETFL);
+        if (opts == -1)
+            return -1;
+
+        fd_was_non_blocking = opts & O_NONBLOCK;
+
+        if (fd_was_non_blocking) {
+            opts &= ~O_NONBLOCK;
+            if (fcntl(fd, F_SETFL, opts) == -1)
+                return -1;
+        }
+
+        return 0;
+    }
+
+    /*
+     * Reset the socket like in the mode (blocking vs. non-blocking) it was.
+     */
+    int unset_blocking() {
+        if (fd_was_non_blocking) {
+            int opts = fcntl(fd, F_GETFL);
+            if (opts == -1)
+                return -1;
+
+            opts |= O_NONBLOCK;
+            if (fcntl(fd, F_SETFL, opts) == -1)
+                return -1;
+        }
+
+        return 0;
+    }
+
+ public:
+    PosixReadWorker(Nan::Callback *callback, int fd, size_t size)
+            : Nan::AsyncWorker(callback), fd(fd), size(size) {
+        buffer = Nan::NewBuffer((uint32_t) size).ToLocalChecked();
+        data = node::Buffer::Data(buffer);
+    }
+
+    ~PosixReadWorker() {}
+
+    /*
+     * Executed inside the worker-thread. It is not safe to access V8, or V8
+     * data structures here, so everything we need for input and output should
+     * go on `this`.
+     */
+    void Execute() {
+        static char msg[256];
+        size_t count = 0;
+
+        // TODO(adrienverge): Double check that it's really not needed to free
+        // buffer when returning in case of error
+        if (set_blocking()) {
+            snprintf(msg, sizeof(msg), "fnctl failed: %s", strerror(errno));
+            SetErrorMessage(msg);
+            return;
+        }
+
+        do {
+            ssize_t n = read(fd, &data[count], size - count);
+            if (n == -1) {
+                if (errno == EINTR)
+                    continue;
+
+                snprintf(msg, sizeof(msg), "read failed: %s", strerror(errno));
+                SetErrorMessage(msg);
+                break;
+            } else if (n == 0) {  // end of stream
+                snprintf(msg, sizeof(msg),
+                        "reached end of stream (read %lu bytes)", count);
+                SetErrorMessage(msg);
+                break;
+            } else {
+                count += n;
+            }
+        } while (count < size);
+
+        if (unset_blocking()) {
+            snprintf(msg, sizeof(msg), "fnctl failed: %s", strerror(errno));
+            SetErrorMessage(msg);
+        }
+    }
+
+    /*
+     * Executed when the async work is complete this function will be run
+     * inside the main event loop so it is safe to use V8 again.
+     */
+    void HandleOKCallback() {
+        Nan::HandleScope scope;
+
+        v8::Local<v8::Value> argv[] = { Nan::Null(), buffer };
+
+        callback->Call(2, argv);
+    }
+};
+
+void Read(const Nan::FunctionCallbackInfo<v8::Value>& info) {
+    if (info.Length() != 3) {
+        Nan::ThrowTypeError("wrong number of arguments");
+        return;
+    }
+
+    /*
+     * Get 'socket' argument.
+     */
+    if (!looks_like_a_socket(info[0])) {
+        Nan::ThrowTypeError("first argument should be a socket");
+        return;
+    }
+    v8::Local<v8::Object> socket = info[0].As<v8::Object>();
+
+    /*
+     * Get 'size' argument.
+     */
+    if (!info[1]->IsNumber() || Nan::To<int>(info[1]).FromJust() <= 0) {
+        Nan::ThrowTypeError("second argument should be a positive integer");
+        return;
+    }
+    size_t size = Nan::To<int>(info[1]).FromJust();
+
+    /*
+     * Get 'callback' argument.
+     */
+    if (!info[2]->IsFunction()) {
+        Nan::ThrowTypeError("third argument should be a function");
+        return;
+    }
+    Nan::Callback *callback = new Nan::Callback(info[2].As<v8::Function>());
+
+    /*
+     * Run-time checks. They don't throw (since these are not programmer errors)
+     * but callback(error).
+     */
+    if (!socket_is_readable(socket)) {
+        v8::Local<v8::Value> argv[] = { Nan::Error("socket is not readable") };
+        callback->Call(1, argv);
+        return;
+    }
+    // Check if the 'socket' argument is well-formed and extract its file
+    // descriptor.
+    int fd = get_fd_from_socket(socket);
+    if (fd == -1) {
+        v8::Local<v8::Value> argv[] = {
+            Nan::Error("malformed socket object, cannot get file descriptor") };
+        callback->Call(1, argv);
+        return;
+    }
+
+    Nan::AsyncQueueWorker(new PosixReadWorker(callback, fd, size));
+    return;
+}
+
+void Init(v8::Local<v8::Object> exports) {
+    exports->Set(Nan::New("read").ToLocalChecked(),
+                 Nan::New<v8::FunctionTemplate>(Read)->GetFunction());
+}
+
+NODE_MODULE(posixread, Init)
---
+++ test/posixread.js
@@ -0,0 +1,175 @@
+const assert = require('assert');
+const crypto = require('crypto');
+const net = require('net');
+
+const posixread = require('../index');
+
+describe('posixread.read()', () => {
+    it('should detect non-socket objects (undefined)', (done) => {
+        try {
+            posixread.read(undefined, 10, () => {});
+            done(new Error('error not thrown'));
+        } catch (err) {
+            if (err instanceof TypeError
+                    && err.message === 'first argument should be a socket')
+                return done();
+            return done(err);
+        }
+    });
+
+    it('should detect non-socket objects (number)', (done) => {
+        try {
+            posixread.read(42, 10, () => {});
+            done(new Error('error not thrown'));
+        } catch (err) {
+            if (err instanceof TypeError
+                    && err.message === 'first argument should be a socket')
+                return done();
+            return done(err);
+        }
+    });
+
+    it('should detect non-socket objects (fake socket)', (done) => {
+        try {
+            const socket = { _handle: { fd: 2 }, readable: true };
+            posixread.read(socket, 10, () => {});
+            done(new Error('error not thrown'));
+        } catch (err) {
+            if (err instanceof TypeError
+                    && err.message === 'first argument should be a socket')
+                return done();
+            return done(err);
+        }
+    });
+
+    it('should detect bad socket (not readable)', (done) => {
+        const socket = new net.Socket();
+        posixread.read(socket, 10, (err) => {
+            if (err === undefined)
+                return done(new Error('error not thrown'));
+            if (err.message !== 'socket is not readable')
+                return done(err);
+            return done();
+        });
+    });
+
+    function getNewSocket(callback) {
+        const otherEnd = new net.Socket();
+
+        const server = net.createServer(
+            { pauseOnConnect: true },
+            function onConnection(socket) {
+                server.close();
+                callback(socket, otherEnd);
+                otherEnd.end();
+                // otherEnd.destroy();
+            });
+
+        server.listen(function onListening() {
+            otherEnd.connect(server.address().port);
+        });
+    }
+
+    it('should detect bad socket (invalid handle)', (done) => {
+        getNewSocket(function onSocket(socket) {
+            socket._handle = {};
+            posixread.read(socket, 10, (err) => {
+                if (err === undefined)
+                    return done(new Error('error not thrown'));
+                if (err.message !== 'malformed socket object, cannot get ' +
+                                    'file descriptor')
+                    return done(err);
+                return done();
+            });
+        });
+    });
+
+    it('should detect bad requested size (wrong type)', (done) => {
+        getNewSocket(function onSocket(socket) {
+            try {
+                posixread.read(socket, 'test', () => {});
+                done(new Error('error not thrown'));
+            } catch (err) {
+                if (err instanceof TypeError
+                        && err.message === 'second argument should be a ' +
+                                           'positive integer')
+                    return done();
+                return done(err);
+            }
+        });
+    });
+
+    it('should detect bad requested size (negative)', (done) => {
+        getNewSocket(function onSocket(socket) {
+            try {
+                posixread.read(socket, -2, () => {});
+                done(new Error('error not thrown'));
+            } catch (err) {
+                if (err instanceof TypeError
+                        && err.message === 'second argument should be a ' +
+                                           'positive integer')
+                    return done();
+                return done(err);
+            }
+        });
+    });
+
+    it('should detect bad callback', (done) => {
+        getNewSocket(function onSocket(socket) {
+            try {
+                posixread.read(socket, 10, 'callback');
+                done(new Error('error not thrown'));
+            } catch (err) {
+                if (err instanceof TypeError
+                        && err.message === 'third argument should be a ' +
+                                           'function')
+                    return done();
+                return done(err);
+            }
+        });
+    });
+
+    it('should read 10 bytes', (done) => {
+        getNewSocket(function onSocket(socket, otherEnd) {
+            otherEnd.write('ABCDEFGHIJKLMNOPQRSTUVWXYZ', () => {
+                posixread.read(socket, 10, (err, buffer) => {
+                    if (err)
+                        return done(err);
+
+                    assert.deepStrictEqual(buffer, new Buffer('ABCDEFGHIJ'));
+                    done();
+                });
+            });
+        });
+    });
+
+    it('should read 90000 bytes', (done) => {
+        getNewSocket(function onSocket(socket, otherEnd) {
+            const bigBuf = crypto.randomBytes(99999);
+            otherEnd.write(bigBuf, () => {
+                posixread.read(socket, 90000, (err, buffer) => {
+                    if (err)
+                        return done(err);
+
+                    assert.deepStrictEqual(buffer, bigBuf.slice(0, 90000));
+                    done();
+                });
+            });
+        });
+    });
+
+    it('should detect end of stream before having read all', (done) => {
+        getNewSocket(function onSocket(socket, otherEnd) {
+            otherEnd.end('123456789', () => {
+                posixread.read(socket, 10, (err) => {
+                    if (err === undefined)
+                        return done(new Error('error not thrown'));
+                    if (err.message !== 'reached end of stream (read 9 bytes)')
+                        return done(err);
+
+                    done();
+                });
+            });
+        });
+    });
+});
